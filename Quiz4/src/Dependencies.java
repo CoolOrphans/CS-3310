708584c7445922a0fc4f0d4d46c2e1f14bd0d760
Nole Serrano <43283288+CoolOrphans@users.noreply.github.com>
2020-10-21 00:26:44 -0700

    Update Dependencies.java

--- Quiz4/src/Dependencies.java
+++ Quiz4/src/Dependencies.java
@@ -3,24 +3,30 @@ import java.util.Scanner;
 public class Dependencies {
 
 	public static void main(String[] args) {
-		print("EXAMPLE PROJECTS: a, b, c, d, e, f\n");
+		print("Example Projects: a, b, c, d, e, f\n");
 
 		Scanner sc = new Scanner(System.in);
 		sc.useDelimiter("\n");
 		print("Projects: ");
 		// EXAMPLE INPUT: a, b, c, d, e, f
 		String Projs = sc.next();
-
+		// use default by just pressing enter
+		if (Projs.length() == 1) {
+			Projs = "a, b, c, d, e, f";
+		}
 		char[] projs = new char[(Projs.length() + 2) / 3];
 		for (int i = 0; i < projs.length; i++) {
 			projs[i] = Projs.charAt(i * 3);
 		}
-		
-		print("EXAMPLE DEPENDENCIES: (a, d), (f, b), (b, d), (f, a), (d, c)\n");
+
+		print("Example Dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)\n");
 		print("Dependencies: ");
 		// EXAMPLE INPUT: (a, d), (f, b), (b, d), (f, a), (d, c)
 		String Deps = sc.next();
-
+		// use default by just pressing enter
+		if (Deps.length() == 1) {
+			Deps = "(a, d), (f, b), (b, d), (f, a), (d, c)";
+		}
 		char[] deps = new char[(Deps.length() + 2) / 7 * 2];
 		int k = 0;
 		for (int i = 0; i < deps.length / 2; i++) {
@@ -36,9 +42,14 @@ public class Dependencies {
 			x[i] = Deps.charAt(i * 8 + 1);
 			y[i] = Deps.charAt(i * 8 + 4);
 		}
+		printChars(x);
+		printChars(y);
 
 		sc.close();
+	}
 
+	// to remove dependencies can swap characters at index and decrease a counter
+	public static void buildOrder(char[] projs, char[] x, char[] y) {
 	}
 
 	// Test function
